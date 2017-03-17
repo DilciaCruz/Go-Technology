6a4070f106816dce8da2d4185e51c8c847cebd5d
Astrid <Astrid@DESKTOP-P3M3BR6>
2017-03-17 12:17:32 -0600

    Merge origin/master
    Conflicts:
    	src/controlador/ManejadorVentanas.java

--- src/controlador/ManejadorVentanas.java
+++ src/controlador/ManejadorVentanas.java
@@ -12,37 +12,53 @@ import vista.*;
  */
 public class ManejadorVentanas {
     
-    public Login loginfrm;
-    public Clientes clientesfrm;
-    public ListaPuestos listaPuestosfrm;
-    
-    public Inventario inventariofrm;
-    public Compras comprasfrm;
-    public Parametros parametrosfrm;
-    public RestablecerClaves restablecerClavefrm;
-    public RegistrarCliente registrarClientefrm;
-    public MenuPrincipal menuPrincipalfrm;
-    public Cotizaciones cotizacionfrm;
-    public NuevaCotización nuevaCotizacionfrm;
-    public Proyectos proyectosfrm;
-    public Factura facturafrm;
     public Agendas agendafrm;
+    public Clientes clientesfrm;
+    public Compras comprasfrm;
+    public Cotizaciones cotizacionfrm;
+    public Factura facturafrm;
+    public ImprimirCotizacion imprimirCotizacionfrm;
+    public Inventario inventariofrm;
+    public ListaParametros listaParametrosfrm;
+    public ListaProyecto listaProyectofrm;
+    public ListaPuestos listaPuestosfrm;
+    public Login loginfrm;
+    public MenuPrincipal menuPrincipalfrm;
+    public NuevaCotización nuevaCotizacionfrm;
+    public NuevaFacturaImpresa nuevaFacturaImpresafrm;
+    public NuevaOrdenCompra nuevaOrdenComprafrm;
+    public OrdenCompraProyecto ordenCompraProyectofrm;
+    public Parametros parametrosfrm;
+    public Proyectos proyectosfrm;
+    public Puestos puestosfrm;
+    public RegistrarCliente registrarClientefrm;
+    public RestablecerClaves restablecerClavefrm;
+    
+    
     
    public ManejadorVentanas(){
-       this.loginfrm = new Login();
-       
-       this.listaPuestosfrm = new ListaPuestos();
-       this.clientesfrm = new Clientes();
-       this.restablecerClavefrm = new RestablecerClaves();
-       this.parametrosfrm = new Parametros();
-       this.inventariofrm = new Inventario();
-       this.comprasfrm = new Compras();
-       this.registrarClientefrm = new RegistrarCliente();
-       this.menuPrincipalfrm = new MenuPrincipal();
-       this.cotizacionfrm = new Cotizaciones();
-       this.nuevaCotizacionfrm = new NuevaCotización();
-       this.proyectosfrm = new Proyectos();
-       this.facturafrm = new Factura();
        this.agendafrm = new Agendas();
+       this.clientesfrm = new Clientes();
+       this.comprasfrm = new Compras();
+       this.cotizacionfrm = new Cotizaciones();
+       this.facturafrm = new Factura();
+       this.imprimirCotizacionfrm = new ImprimirCotizacion();
+       this.inventariofrm = new Inventario();
+       this.listaParametrosfrm = new ListaParametros();
+       this.listaProyectofrm = new ListaProyecto(); 
+       this.listaPuestosfrm = new ListaPuestos(); 
+       this.loginfrm = new Login(); 
+       this.menuPrincipalfrm = new MenuPrincipal(); 
+       this.nuevaCotizacionfrm = new NuevaCotizacion();
+       this.nuevaFacturaImpresafrm = new NuevaFacturaImpresa(); 
+       this.nuevaOrdenComprafrm = new NuevaOrdenCompra(); 
+       this.ordenCompraProyectofrm = new OrdenCompraProyecto(); 
+       this.parametros = new Parametros(); 
+       this.proyectos = new Proyectos();
+       this.puestos = new Puestos();
+       this.registrarCliente = new RegistrarCliente();
+       this.registrarEmpleado = new RegistrarEmpleado();
+       this.restablecerClave = new RegistrarClaves();
+       
    }
 }

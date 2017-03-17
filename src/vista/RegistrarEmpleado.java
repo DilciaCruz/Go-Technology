5f07352164b7ccac005905825f3f05386ff99340
Rosa Sandoval <Rosa Sandoval@DESKTOP-JIDU3U8>
2017-03-17 11:32:28 -0600

    Merge origin/master
    Conflicts:
    	src/modelo/MantenimientoEmpleados.java
    	src/vista/RegistrarEmpleado.java

--- src/vista/RegistrarEmpleado.java
+++ src/vista/RegistrarEmpleado.java
@@ -163,6 +163,12 @@ public class RegistrarEmpleado extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(204, 204, 204));
 
+        cmbEstado.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbEstadoActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -197,6 +203,7 @@ public class RegistrarEmpleado extends javax.swing.JFrame {
 
         jLabel12.setText("Cargo");
 
+        cmbCargo.setKeySelectionManager(null);
         cmbCargo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmbCargoActionPerformed(evt);
@@ -447,6 +454,10 @@ public class RegistrarEmpleado extends javax.swing.JFrame {
         // TODO add your handling code here:
        
     }//GEN-LAST:event_cmbCargoActionPerformed
+
+    private void cmbEstadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbEstadoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmbEstadoActionPerformed
 
     /**
      * @param args the command line arguments
